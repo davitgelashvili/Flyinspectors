fcb754effe51ca329d73c4a643c22aa5dd11094e
Davit Gelashvili <datoogelashvili@gmail.com>
2025-06-25 17:46:09 +0400

    hide /show profile info

--- tourclaimcom/src/components/Admin/Profile.js
+++ tourclaimcom/src/components/Admin/Profile.js
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
-import React from 'react'
+import React, { useState } from 'react'
 import icon from './../../assetss/profileicon.jpeg'
 
 export const Profile = () => {
     const { userData } = useSelector(state => state.userData)
+    const [show, setShow] = useState(false)
 
     const profile = {
         width: '70px',
@@ -17,17 +18,28 @@ export const Profile = () => {
         img: {
             width: '100%',
             height: '100%'
+        },
+
+        p: {
+            margin: 0,
         }
     }
 
     return (
         <div className="d-flex align-items-center">
             <div style={profile}>
-                <img src={icon} alt="icon" style={profile.img}/>
+                <img src={icon} alt="icon" style={profile.img} />
             </div>
             <div>
-                <p>name: {userData.title}</p>
-                <p>ID: {userData.companyId}</p>
+                <p style={profile.p}>name: {userData.title}</p>
+                <p style={profile.p}>ID: {userData.companyId}</p>
+                <p style={profile.p}>userName: {show ? userData.userName : '********'}</p>
+                <p style={profile.p}>password: {show ? userData.password : '********'}</p>
+                <div>
+                    <button onClick={() => setShow(!show)}>
+                        {show ? 'hide' : 'show'}
+                    </button>
+                </div>
             </div>
         </div>
     )
